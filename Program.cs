87c9b16fe5c54d3c06a997ccb9e9897c287b123f
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-03-31 18:59:55 -0300

    add project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Digite seu Nome: ");
+nome = Console.ReadLine();
+Console.Write("Digite o Sobrenome: ");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.Write($"Nome completo: {sobrenome.ToUpper()}, {nome}");
+
